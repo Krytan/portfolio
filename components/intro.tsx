6806b13f2120ec9344e25467b1f7d7dd4e9d4902
GHOSTW <krytanx@gmail.com>
2023-12-27 16:12:26 +0100

    edited intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -76,7 +76,18 @@ export default function Intro() {
           delay: 0.1,
         }}
       >
-        <Link
+
+        
+        <motion.div
+  className="flex items-center justify-center gap-2" // Added a gap of 4 units for spacing
+  initial={{ opacity: 0, y: 100 }}
+  animate={{ opacity: 1, y: 0 }}
+  transition={{
+    delay: 0.1,
+  }}
+>
+
+<Link
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
           onClick={() => {
@@ -87,32 +98,58 @@ export default function Intro() {
           Contact me here{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
+        
+  <a
+    className="group bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+    href="https://www.linkedin.com/in/luanfaber/"
+    target="_blank"
+  >
+    <BsLinkedin />
+  </a>
+
+  <a
+    className="group bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+    href="https://github.com/Krytan"
+    target="_blank"
+  >
+    <FaGithubSquare />
+  </a>
+</motion.div>
+      </motion.div>
+
+      <motion.div
+        className="flex items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+
+<motion.h1
+        className="mb-8 mt-6 px-1 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="text-sm opacity-60">Also feel free to download my</span>
+        
+      </motion.h1>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          className="group bg-white px-5 py-3 flex items-center gap-1 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="/LuanFaberCV.pdf"
           download
         >
-          Download CV{" "}
+          CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
-        <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/luanfaber/"
-          target="_blank"
-        >
-          <BsLinkedin />
-        </a>
 
-        <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/Krytan"
-          target="_blank"
-        >
-          <FaGithubSquare />
-        </a>
+
+
       </motion.div>
+
+
     </section>
   );
 }
